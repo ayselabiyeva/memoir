3a766605a77df775dda88df453e3a2d31034fd65
Aysel Abiyeva <ayselabiyeffam13@gmail.com>
2026-04-07 16:52:19 +0300

    some errors solved

--- app/src/main/java/memoir/App.java
+++ app/src/main/java/memoir/App.java
@@ -2,27 +2,56 @@ package memoir;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import memoir.model.Note;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+      public static void connectDB() {
 
-    public static void connect() {
+            String url = "jdbc:sqlite:./memoir.db";
 
-        String url = "jdbc:sqlite:./memoir.db";
-
-        try (var conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                System.out.println("Connection to SQLite has been established.");
+            try (var conn = DriverManager.getConnection(url)) {
+                if (conn != null) {
+                    System.out.println("Connection to SQLite has been established.");
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+      }
+    public String getGreeting(){
+        return "Hello";
     }
 
+
+  
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        connect();
+        connectDB();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter note id: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter note title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter note content: ");
+        String content = scanner.nextLine();
+
+        System.out.print("Enter name who writes note?: ");
+        String creator = scanner.nextLine();
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Note note = new Note(id, title, content, now, creator);
+
+        System.out.println("\nCreated note:");
+        System.out.println(note);
+
+        scanner.close();
+
     }
 }
+
